951cc13e63974ef7cea6a7b81f97c03fcecf51d4
TaniaPedrazaG <taniapedraza97@gmail.com>
2020-06-28 19:58:59 -0500

    Se agregaron todos los ejercicios con susrespectivas validaciones

--- main.c
+++ main.c
@@ -2,25 +2,89 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-//Inicialización de metodos
+#include <stdbool.h>
+#include <math.h>
+#define TAM 10000
+/* INICIALIZACION DE METODOS */
+
+//1.NumerosPrimos
 void cousinNumbers();
 void calculateCousinNumbers(int);
 int isCousing(int);
 
-//metodos
-void cousinNumbers() {
-    int number;
-    printf("Calculate Cousin Numbers \n");
-    printf("Digite el valor:\n");
-    //scanf("%i", &number);
+//2. Numero Ególatra
+void egotisticalNumber();
+int processEgoistcalNumber(int number);
+int isEgoistcalNumber(int number);
 
-    if(scanf("%i", &number) != 1 || number<0 ) {
-        printf("Caracter no válido, por favor digite un entero\n");
-    }else{
-        printf("Los numeros primos de acuerdo al valor %d, son: \n", number);
-        calculateCousinNumbers(number);
+//3. Numeros Magicos
+void magicalNumber();
+void vector(int numbers[], char chain[], int tamano);
+int sortDescending(int numbers[], int tamano);
+int sortAscending(int numbers[], int tamano);
+int convertVector(int numbers[], int tamano);
+int isMagicNumber(int numberD, int numberA, int number);
+
+//4. Calculo del IMC
+void bodyMassIndex();
+double calculateIMC(int weight, double height);
+char * processIMC(int gender, double IMC);
+
+//5.Suma de Digitos
+void sumDigits();
+void calculateSumDigits(int iniInterval, int endInterval, int exclusiveDigit);
+
+//6. Serie Fibonacci
+void calcFibonacciSerie(int number, char patron []);
+void fibonacciSeries();
+
+//7. Resultados de prueba
+void test();
+int calculateTest(char* chain);
+
+/*METODOS*/
+//1. Números Primos
+bool validationReal( char number[]){
+
+    for (int i = 0; i < strlen(number); ++i) {
+        if (!(isdigit(number[i]))){
+            printf("<----Ingresa solo numeros enteros positivos---->\n");
+            return true;
+        }
+
     }
+
+    return false;
+}
+
+
+int validations(){
+    char number [30];
+    int valido=0;
+    do {
+        scanf("%s",number);
+
+    }while (validationReal(number)==true);
+
+    valido= atoi(number);
+
+    return valido;
+}
+
+void cousinNumbers() {
+    int number=0;
+    printf("---Numeros Primos---\n");
+    printf("Digite un numero: \n");
+    fflush(stdin);
+    number=validations();
+    fflush(stdin);
+
+
+    printf("Los numeros primos de acuerdo al valor %d, son: \n", number);
+    calculateCousinNumbers(number);
     getchar();
+
+
 }
 
 void calculateCousinNumbers(int number) {
@@ -41,42 +105,388 @@ int isCousing(int number) {
     }
     return cousing;
 }
-//terminar metodos primos
 
-void calcPrimeNumbers(){
-    printf("Selecciono la opcion 1\n");
+//2. Número Ególatra
+
+
+void egotisticalNumber() {
+    int number;
+    printf("Calculo de un Numero Egolatra \n");
+    printf("Digite el numero entero a evaluar: \n");
+    number = validations();
+
+    if(isEgoistcalNumber( number)){
+        printf("El numero %d es un Ególatra\n", number);
+    }else{
+        printf("El numero %d NO es un Ególatra\n", number);
+    }
+    getchar();
+
+}
+
+int processEgoistcalNumber(int number){
+    int suma, num=0;
+    char chain[100];
+    sprintf(chain, "%d", number);
+
+    for(int i = 0; i <= strlen(chain)-1; i++){
+        num = chain[i] - '0';
+        suma+= pow(num, 3);
+    }
+    return suma;
+}
+
+int isEgoistcalNumber(int number){
+    if(processEgoistcalNumber(number) == number){
+        return 1;
+    }
+    return 0;
+}
+//3. Números Magicos
+bool validationRealMagico( char number[]){
+
+    for (int i = 0; i < strlen(number); ++i) {
+
+        if (!(number[0]==45||number[i]>=48&&number[i]<=57)){
+            printf("<----Ingresa solo numeros enteros---->\n");
+            return true;
+        }
+
+    }
+
+    return false;
+}
+
+int validationMagico(){
+    char number [30];
+    int valid=0;
+    do {
+        scanf("%s",number);
+
+    }while (validationRealMagico(number)==true);
+
+    valid= atoi(number);
+
+    return valid;
+}
+
+
+
+void magicalNumber() {
+    int number;
+    int numbers[100];
+
+    printf("Calcular si el Numero es Magico\n");
+    printf("Digite el valor \n");
+
+    number=validationMagico();
+
+    char chain[100];
+    sprintf(chain, "%d", number);
+    int tamano=strlen(chain);
+
+    vector(numbers, chain, tamano); //Agregar los digitos del número en un vector
+    //Ordena los digitos del vector, lo vueve a convertir en cadena luego en numero y los resta
+    if(isMagicNumber(sortDescending(numbers, tamano),sortAscending(numbers, tamano), number)){
+        printf("El numero %d es un Número Magico\n", number);
+    }else{
+        printf("El numero %d No es un Número Magico\n", number);
+    }
+    getchar();
+
+}
+void vector(int numbers[], char chain[], int tamano){
+    for (int i = 0; i <= tamano - 1; i++) {
+        int num = chain[i] - '0';
+        numbers[i] = num;
+    }
+}
+
+int isMagicNumber(int numberD, int numberA, int number){
+    if((numberD-numberA)==number){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int sortAscending(int number[], int tamano) {
+    //printf("ordenar \n");
+    for (int i = 0; i < tamano - 1; i++) {
+        for (int j = i + 1; j < tamano; j++) {
+            if (number[j] < number[i]) {
+                int aux = number[j];
+                number[j] = number[i];
+                number[i] = aux;
+            }
+        }
+    }
+    return convertVector(number, tamano);;
+}
+
+int convertVector(int numbers[], int tamano){
+    char cadena[30]="";
+
+    for(int i=0; i<tamano; i++){
+        numbers[i];
+        char convNum[10];
+        sprintf(convNum, "%d", numbers[i]);
+        strcat(cadena, convNum);
+    }
+    //  printf("%s\n",  cadena);
+    int num = atoi(cadena);
+
+    return num;
+}
+
+int sortDescending(int numberA[], int tamano){
+    // printf("ordenar \n");
+    for (int i = 0; i < tamano-1; i++){
+        for (int j = i + 1; j < tamano; j++){
+            if(numberA[j]>numberA[i]){
+                int aux= numberA[j];
+                numberA[j]=numberA[i];
+                numberA[i]= aux;
+            }
+        }
+    }
+
+    return convertVector(numberA, tamano);
+}
+//4. Calculo IMC
+
+bool validacionReakGener( char number[]){
+
+    for (int i = 0; i <strlen(number) ; ++i) {
+        if (!(number[i]==49||number[i]==50)){
+
+            printf("<---Digita solamente 1 o 2 ---->");
+
+            return true;
+        }
+
+
+    }
+    return false;
+
+
+}
+int validationGener(){
+    char number [30];
+    int value=0;
+    do {
+        scanf("%s",number);
+    }while (validacionReakGener(number)==true);
+    value=atoi(number);
+
+    return  value;
+
+}
+void bodyMassIndex(){
+    int weight, gender;
+    double height;
+
+    printf("Calculo del IMC\n");
+    printf("Digite su peso en kg: \n");
+
+    weight=validations();
+    printf("Digite su estatura en cm: \n");
+
+    height=validations();
+    printf("Digite '1' si es Mujer o '2' si es Hombre: \n");
+
+    gender=validationGener();
+    printf("Su diagnostico es: %s\n", processIMC(gender, calculateIMC(weight, height)));
     getchar();
 }
 
-void calcEgotisticalNumber(){
-    printf("Selecciono la opcion 2\n");
+char* processIMC (int gender, double IMC){
+
+    switch (gender){
+
+        case 1:
+            if(IMC < 16){
+                return "Desnutrición";
+            } else if ((IMC >= 16) && (IMC <= 20)){
+                return "Bajo Peso";
+            } else if ((IMC > 20) && (IMC <= 24)){
+                return "Normal";
+            } else if ((IMC > 24) && (IMC <= 29)){
+                return "Sobrepeso";
+            }else if((IMC > 29) && (IMC <= 34)){
+                return "Obesidad";
+            }else if((IMC > 34) && (IMC <= 39)){
+                return "Obesidad Marcada";
+            }else if(IMC > 39){
+                return "Obesidad Morbida";
+            }
+            break;
+
+        case 2:
+            if(IMC < 17){
+                return "Desnutricion";
+            } else if ((IMC >= 17) && (IMC <= 20)){
+                return "Bajo Peso";
+            } else if ((IMC > 20) && (IMC <= 25)){
+                return "Normal";
+            } else if ((IMC > 25) && (IMC <= 30)){
+                return "Sobrepeso";
+            }else if((IMC > 30) && (IMC <= 35)){
+                return "Obesidad";
+            }else if((IMC > 35) && (IMC <= 40)){
+                return "Obesidad Marcada";
+            }else if(IMC > 40){
+                return "Obesidad Morbida";
+            }
+            break;
+
+        default:
+            return "ERROR. Opcion no valida, intentelo nuevamente";
+
+    }
+}
+
+double calculateIMC(int weigth, double height){
+    return weigth/(pow((height*0.01),2));
+}
+
+//5.Suma de Digitos
+
+void sumDigits(){
+    int iniInterval=0;
+    int endInterval=0;
+    int exclusiveDigit=0;
+    printf("<----Suma de digitos---->\n");
+    printf("Ingresa el intervalo de inicio:\n");
+    iniInterval=validations();
+    fflush(stdin);
+    printf("Ingresa el intervalo final:\n");
+    endInterval=validations();
+    fflush(stdin);
+    printf("Digite el numero que desea excluir:\n");
+    exclusiveDigit=validations();
+    fflush(stdin);
+
+
+    iniInterval<=endInterval?calculateSumDigits(iniInterval, endInterval, exclusiveDigit):printf("Lo siento el Intervalo Inicial debe ser menor\n");
     getchar();
 }
 
-void calcMagicNumber(){
-    printf("Selecciono la opcion 3\n");
+void calculateSumDigits(int iniInterval, int endInterval, int exclusiveDigit){
+    char* numberC;
+    int cont=0, suma=0;
+    for(int i=iniInterval; i<=endInterval; i++){
+        sprintf(numberC, "%d", i);
+        for(int j=0; strlen(numberC)>j; j++){
+            int chainConv = numberC[j] - '0';
+            if(chainConv==exclusiveDigit){
+                cont++;
+            }else{
+                suma+=chainConv;
+            }
+        }
+    }
+
+    printf("\nLos intervalos %d y %d sin tener en cuenta el %d, la suma sería %d y no se tuvieron en cuenta %d digitos\n",
+           iniInterval, endInterval, exclusiveDigit, suma, cont );
+}
+
+//7. Resultados de la prueba
+bool validationRealtest( char number[]){
+
+
+    for (int i = 0; i < strlen(number); ++i) {
+
+        if (!(number[i]==88||number[i]==79||number[i]==120||number[i]==111)){
+
+            printf("<----Ingresa solo O || X---->\n");
+            return true;
+        }
+
+    }
+
+    return false;
+}
+
+
+void test (){
+
+    printf("----Puntaje de prueba---- \n");
+    char chain[30] ;
+    printf("Digita la cadena por favor: \n");
+
+    do {
+        scanf("%s", chain);
+
+    }while (validationRealtest(chain)==true);
+
+
+    printf("El resultado de la prueba dada es: %d\n",calculateTest(chain));
+    getchar();
+
+}
+
+int calculateTest(char* chain){
+    int  sumaO=0, suma=0;
+    char digitC;
+    for(int i=0; i<strlen(chain); i++){
+        digitC=chain[i];
+        digitC=toupper(digitC);
+        if(digitC=='O'){
+            sumaO++;
+        }else if(digitC=='X'){
+            sumaO=0;
+        }
+        suma+=sumaO;
+    }
+    return suma;
+}
+
+
+void calcFibonacciSerie(int number, char patron []){
+    char fib0[TAM] = "0";
+    char fib1[TAM] = "1";
+
+    printf("***** SERIE FIBONACCI *****\n");
+    for (int i = 2; i <= number; i++) {
+        char fibNuevo[TAM] = "";
+        strcat(fibNuevo, fib1);
+        strcat(fibNuevo, fib0);
+        strcpy(fib0,fib1);
+        strcpy(fib1,fibNuevo);
+        printf("f(%d) = %s\n", i, fibNuevo);
+    }
+
+    char* secuence = patron;
+    char* search = NULL;
+
+    int cont = 0;
+    search = strstr(fib1, secuence);
+    while (search != NULL){
+        cont++;
+        search = strstr(search + 1, secuence);
+    }
+    printf("El patron %s esta %i veces en la serie %s\n", secuence, cont, fib1);
+}
+
+void fibonacciSeries(){
+    int number;
+    char patron[30];
+    printf("Digite el numero de la Secuencia Fibonacci ha generar: ");
+    number=validations();
+
+    printf("Digite el patron ha buscar: ");
+    do {
+        scanf("%s",patron);
+
+    }while (validationReal(patron)==true);
+    printf("En el metodo para arrancar\n");
+
+    calcFibonacciSerie(number, patron);
     getchar();
 }
 
-void calcIMC(){
-    printf("Selecciono la opcion 4\n");
-    getchar();
-}
 
-void calcSumOfDigits(){
-    printf("Selecciono la opcion 5\n");
-    getchar();
-}
-
-void calcFibonacciSequence(){
-    printf("Selecciono la opcion 6\n");
-    getchar();
-}
-
-void calcTestScore(){
-    printf("Selecciono la opcion 7\n");
-    getchar();
-}
 
 void mainMenu(){
     int option;
@@ -105,27 +515,27 @@ void mainMenu(){
                 break;
 
             case 2:
-                calcEgotisticalNumber();
+                egotisticalNumber();
                 break;
 
             case 3:
-                calcMagicNumber();
+                magicalNumber();
                 break;
 
             case 4:
-                calcIMC();
+                bodyMassIndex();
                 break;
 
             case 5:
-                calcSumOfDigits();
+                sumDigits();
                 break;
 
             case 6:
-                calcFibonacciSequence();
+                fibonacciSeries();
                 break;
 
             case 7:
-                calcTestScore();
+                test();
                 break;
 
             case 8:
